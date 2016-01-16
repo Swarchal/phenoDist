e76cc887a7b168e55279d6a2a4bb5f885fcb7f67
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-01-16 14:17:17 +0000

    fix matrix/database check

--- R/average_vector.R
+++ R/average_vector.R
@@ -14,12 +14,10 @@
 average_vector <- function(x){
 
 	# check input
-	if (is.matrix(x) == FALSE){
-		if (is.data.frame(x) == TRUE){
-			x <- as.matrix(as.numeric(x))
-		} else (stop(call. = FALSE,
-			"x needs to be either a matrix or a numeric dataframe"))
+	if (!is.matrix(x) && !is.data.frame(x)){
+		stop("Input needs to be a dataframe or a matrix")
 	}
+
 	# data will be in the form of rows = vectors, columns = components
 	means <- as.vector(colMeans(x))
 	return(means)
