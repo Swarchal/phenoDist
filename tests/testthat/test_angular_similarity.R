468cd80a8365af6afa821c1af2498efd92dbad41
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-01-16 15:53:11 +0000

    add angular_similarity test

---
+++ tests/testthat/test_angular_similarity.R
@@ -0,0 +1,19 @@
+context("angular_similarity works as expected")
+
+test_that("angular similarity stops when given incorrect input",{
+	expect_error(angular_similarity(1, 2))
+	expect_error(angular_similarity(c(1, 2), 1))
+	expect_error(angular_similarity(c("string"), c(1, 2)))
+	expect_error(angular_similarity(c("string", "string"), c("string", "string")))
+	expect_error(angular_similarity(c(1, 2, 3), c(1, 2)))
+})
+
+test_that("angular similarity returns expected answers",{
+	a <- angular_similarity(c(1, 2), c(1, 2))
+	expect_equal(a ,1)
+
+	b <- angular_similarity(c(10, 10, 10), c(10, 10, 10))
+	expect_equal(a, b)
+
+	expect_equal(angular_similarity(c(1, 0), c(-1, 0)), 0)
+})
