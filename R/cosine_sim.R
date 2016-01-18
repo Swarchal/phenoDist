22d0a08f4dc97c344229714ba194ecc465b6d28d
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-01-18 14:46:05 +0000

    fix spacing

--- R/cosine_sim.R
+++ R/cosine_sim.R
@@ -21,7 +21,7 @@ cosine_sim_mat <- function(X){
 	cos_sim <- function(ix){
     A = X[ix[1],]
     B = X[ix[2],]
-    return( sum(A*B)/sqrt(sum(A^2)*sum(B^2)) )
+    return(sum(A * B) / sqrt(sum(A^2) * sum(B^2)))
 	}
 
 	# if given a dataframe, will try and convert to a matrix
