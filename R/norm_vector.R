2c1e9dd23e2f15db0079ce70e1f0f8b2fccd50ce
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-04-15 16:36:43 +0100

    speed up, get rid of pointless variable assignment

--- R/norm_vector.R
+++ R/norm_vector.R
@@ -17,17 +17,12 @@
 #' norm_vector(x)
 
 norm_vector <- function(x){
-
-	if (is.numeric(x) == FALSE){
-		stop(call. = FALSE,
-			"Input needs to be a number.")
-	}
-	if (length(x) < 2){
+	
+    if (length(x) < 2){
 		stop(call. = FALSE,
 			"Needs to be a vector of a least 2 elements.")
-	}
+    }
 
-	# calculate the length (norm) of a vector
-	norm_vec_out <- as.vector(sqrt(x %*% x))
-	return(norm_vec_out)
+    # calculate the length (norm) of a vector
+    as.vector(sqrt(x %*% x))
 }
--- R/theta.R
+++ R/theta.R
@@ -21,10 +21,5 @@
 
 theta <- function(a, b){
     
-    # computes the angle bewteen two vectors ('a' and 'b')
-    norm_vec <- function(x) sqrt(x %*% x)
-    
-    theta <- as.vector(
-        acos(a %*% b / (norm_vec(a) * norm_vec(b)))) * 180/pi
-    return(theta)
+    as.vector(acos(a %*% b / (norm_vector(a) * norm_vector(b)))) * 180/pi
 }
