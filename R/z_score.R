ca4cbcf0a8683a8ab5d2b5d2e84484fbad21cfb8
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-05-03 17:17:52 +0100

    add zscore functions

---
+++ R/z_score.R
@@ -0,0 +1,25 @@
+#' Z-score
+#'
+#' Scale and center data, essentially the same as \code{scale()}
+#' 
+#' @param x vector of numbers
+#' @param ... additional arguments for mean and sd
+#' @export
+#' 
+zscore <- function(x, ...){
+    (x - mean(x, ...)) / sd(x, ...)
+}
+
+#' Robust Z-score 
+#' 
+#' Robust version of the Z-score using the median and MAD rather than the
+#' mean and standard deviation of the original metric. It is more resiliant
+#' to outliers.
+#' 
+#' @param x vector of numbers
+#' @param ... additional arguments for median and mad
+#' @export
+
+r_zscore <- function(x, ...){
+    (x - median(x, ...)) / mad(x, ...)
+}
---
+++ tests/testthat/test_zscore.R
@@ -0,0 +1,15 @@
+context("z_scores")
+
+test_that("zscore returns errors",{
+
+    expect_error(zscore(c("a", "B", 2, 4)))
+    expect_error(r_zscore(c("a", "B", 2, 4)))
+})
+
+test_that("zscore returns same values as scale",{
+    data <- iris[,1]
+    zscore_out <- zscore(data)
+    scale_out <- scale(data)
+    attributes(scale_out) <- NULL
+    expect_equal(zscore_out, scale_out)
+})
